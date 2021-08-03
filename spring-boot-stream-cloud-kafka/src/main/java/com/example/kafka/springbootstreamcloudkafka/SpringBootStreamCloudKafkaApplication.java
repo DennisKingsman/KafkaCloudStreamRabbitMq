e87f6d06404a991233472e2c46c74f13602c96dd
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-08-04 03:48:32 +0400

    init

---
+++ spring-boot-stream-cloud-kafka/src/main/java/com/example/kafka/springbootstreamcloudkafka/SpringBootStreamCloudKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka.springbootstreamcloudkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStreamCloudKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStreamCloudKafkaApplication.class, args);
+	}
+
+}
